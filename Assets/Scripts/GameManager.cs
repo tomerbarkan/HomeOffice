efac42dd32f510759cf45532aac642e8a22366b4
Tomer <tomerbarkan@gmail.com>
2019-01-25 13:39:52 +0200

    Added input handler

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public Character player;
+	public new Camera camera;
+
+	public PlayerInputHandler playerInput;
+
+    // Start is called before the first frame update
+    void Start() {
+		playerInput = new PlayerInputHandler(player, player.transform.position, camera);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		playerInput.HandleInput();
+    }
+}
---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputHandler {
+	protected Character player;
+	protected Vector3 playerPosition;
+	protected Camera camera;
+
+	public PlayerInputHandler(Character player, Vector3 playerPosition, Camera camera) {
+		this.player = player;
+		this.playerPosition = playerPosition;
+		this.camera = camera;
+	}
+
+	public void HandleInput() {
+		if (Input.GetMouseButtonDown(0)) {
+			Vector2 playerScreenPoint = camera.WorldToScreenPoint(playerPosition);
+			Vector2 toMouse = (Vector2)Input.mousePosition - playerScreenPoint);
+			float angle = Vector2.Angle(Vector2.right, toMouse);
+			float force = Mathf.Clamp(toMouse.magnitude, 0, 1);
+			player.Throw(angle, force);
+		}
+	}
+}
