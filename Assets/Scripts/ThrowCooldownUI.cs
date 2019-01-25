95715855e718b27fcfbd95c541b03b3dfbf4011d
Gavra Games <GavraGames@gmail.com>
2019-01-25 15:22:23 +0200

    Added lotttt of UI stuff

---
+++ Assets/Scripts/ThrowCooldownUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThrowCooldownUI : MonoBehaviour
+{
+
+    public Image fillBar;
+
+    public void Set(float percent)
+    {
+        this.fillBar.fillAmount = percent;
+    }
+
+
+    [ContextMenu("Try To Set")]
+    void SetTry()
+    {
+        Set(0.5f);
+    }
+}
