90de6fbb2e00bb79c4b84f6f70f25e9f62681ede
Gavra Games <GavraGames@gmail.com>
2019-01-25 13:48:27 +0200

    Added character and Throwable

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,9 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Character : MonoBehaviour {
-	public void Throw(float angle, float force) {
+public class Character : MonoBehaviour, IHitable
+{
+    public UnityEvent onHit { get; protected set; }
 
-	}
+   
+
+    [SerializeField]
+    protected Throwable currentThrowable;
+    [SerializeField]
+    protected Transform throwableSpawnPoint;
+
+    public void Hit()
+    {
+        onHit.Invoke();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    
+    //}
+
+    //bool CheckInputFire()
+    //{
+    //    return true;
+    //}
+
+    public void Throw(float angle, float force)
+    {
+        Throwable spawnedThrowable = Throwable.Instantiate<Throwable>(currentThrowable);
+        spawnedThrowable.transform.position = throwableSpawnPoint.position;
+        spawnedThrowable.Throw(angle, force);
+    }
 }
---
+++ Assets/Scripts/Throwable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public abstract class Throwable : MonoBehaviour
+{
+    [SerializeField]
+    protected Rigidbody throwableRigidbody;
+    // Start is called before the first frame update
+    protected  virtual void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        
+    }
+
+    public void Throw(float angle, float force)
+    {
+
+    }
+
+    
+}
